b67ddad60dbbddd31b8888b1169b000b662d6dac
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-04-21 12:41:53 -0300

    Added Img build step to Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 var eslint = require('gulp-eslint');
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 var plumber = require('gulp-plumber');
 var rollup = require('gulp-rollup');
 var sass = require('gulp-sass');
@@ -18,13 +19,13 @@ var util = require('gulp-util');
 var r_babel = require('rollup-plugin-babel');
 var r_commonJS = require('rollup-plugin-commonjs');
 var r_nodeResolve = require('rollup-plugin-node-resolve');
-var r_replace= require('rollup-plugin-replace');
+var r_replace = require('rollup-plugin-replace');
 
 //  Rollup configuration - Based on Qualy Boilerplate's [https://github.com/Qualy-org/qualy-front]
 var ROLLUP_CONFIG = {
   plugins: [
     r_babel({
-      presets: ['react','es2015-rollup'],
+      presets: ['react', 'es2015-rollup'],
       exclude: 'node_modules/**'
     }),
     r_nodeResolve({
@@ -42,15 +43,17 @@ var ROLLUP_CONFIG = {
 //  Object to store the paths to later use
 var PATHS = {
   source: {
-    js: ['src/js/**/*.js','src/js/**/*.jsx'],
+    js: ['src/js/**/*.js', 'src/js/**/*.jsx'],
     jsEntry: 'src/js/app.jsx',
     stylesheets: 'src/scss/**/*.scss',
-    views: 'src/views/**/*.html'
+    views: 'src/views/**/*.html',
+    img: 'src/img/**/*'
   },
   build: {
     js: 'build/js/',
     stylesheets: 'build/css/',
-    views: 'build/'
+    views: 'build/',
+    img: 'build/img/'
   }
 };
 
@@ -71,19 +74,29 @@ gulp.task('css', function () {
   gulp
     .src(PATHS.source.stylesheets)
     .pipe(plumber())
-    .pipe(sass().on('error',sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(util.env.production ? cssnano() : util.noop()) //  If production flag set, we use CSS nano. If not, go ahead
     .pipe(gulp.dest(PATHS.build.stylesheets));
 });
 //  Views build task
-gulp.task('copy', function () {
-    return gulp.src(PATHS.source.views)
-        .pipe(gulp.dest(PATHS.build.views));
+gulp.task('html', function () {
+  return gulp.src(PATHS.source.views)
+    .pipe(gulp.dest(PATHS.build.views));
 });
 
-gulp.task('watch', function (){
-    gulp.watch(PATHS.source.js, ['js']);
-    gulp.watch(PATHS.source.stylesheets, ['css']);
-    gulp.watch(PATHS.source.views, ['copy']);
+gulp.task('img', () => {
+  gulp.src(PATHS.source.img)
+    .pipe(plumber())
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest(PATHS.build.img));
 });
-
+gulp.task('watch', function () {
+  gulp.watch(PATHS.source.js, ['js']);
+  gulp.watch(PATHS.source.stylesheets, ['css']);
+  gulp.watch(PATHS.source.views, ['html']);
+  gulp.watch(PATHS.source.img, ['img']);
+});
