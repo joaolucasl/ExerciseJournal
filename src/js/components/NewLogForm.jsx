a24afcc27db354069848d57be28413d74211f57e
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-04-25 13:03:23 -0300

    Added DatePicker dependency for NewLogForm

--- src/js/components/NewLogForm.jsx
+++ src/js/components/NewLogForm.jsx
@@ -1,38 +1,54 @@
 import React from 'react';
 import Log from '../classes/Log.js';
 import Modal from 'react-modal';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 class NewLogForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       modalIsOpen: false,
+      selectedDate: moment(),
     };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
-  handleFormSubmission(data) {
-
+  handleFormSubmission(event) {
+    event.preventDefault();
   }
   openModal() {
     this.setState({
       modalIsOpen: true,
+      selectedDate: this.state.selectedDate,
     });
   }
   closeModal() {
     this.setState({
       modalIsOpen: false,
+      selectedDate: this.state.selectedDate,
     });
   }
   render() {
     return (
-      <div id="new-log-form">
-        <button onClick={this.openModal}>Add New</button> 
+      <div id="new-log-area">
+        <button onClick={this.openModal}>Add New</button>
         <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal}>
-            <form onSubmit={this.handleFormSubmission}>
+            <form onSubmit={this.handleFormSubmission} id="new-log-form">
               <input type="text" id="time" placeholder="Time spent" />
-              <input type="text" id="time" placeholder="Activity Type" />
-              <button type="submit">Submit</button>
+              <select ref="type" id="type">
+                <option value="run">Running</option>
+                <option value="swim">Swim</option>
+                <option value="bike">Cycling</option>
+              </select>
+              <DatePicker
+                maxDate={moment()}
+                selected={this.state.selectedDate}
+                placeholderText="Date"
+              />
+              <div className="action-buttons">
+                <button type="submit">Submit</button>
+              </div>
             </form>
         </Modal>
       </div>
