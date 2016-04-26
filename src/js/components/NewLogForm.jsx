8c9f0de4d4d186f16416635bc43a5503c1a06b0a
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-04-26 00:02:23 -0300

    Added NewLogForm submission functionality

--- src/js/components/NewLogForm.jsx
+++ src/js/components/NewLogForm.jsx
@@ -12,11 +12,29 @@ class NewLogForm extends React.Component {
       selectedDate: moment(),
       validationError: [],
     };
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
+    // this.openModal = this.openModal.bind(this);
+    // this.closeModal = this.closeModal.bind(this);
+    this.handleFormSubmission = this.handleFormSubmission.bind(this);
+    this.addLog = this.props.addLog;
   }
   handleFormSubmission(event) {
     event.preventDefault();
+    this.clearErrorMsg();
+    const time = Number(this.refs.time.value);
+    const type = this.refs.type.options[this.refs.type.selectedIndex].value;
+    const date = this.state.selectedDate;
+    //  Here we try to create a new log (which may throw exceptions) and add it to the state.
+    try {
+      const tempLog = new Log({
+        time,
+        type,
+        date,
+      });
+      this.addLog(tempLog);
+    } catch (ex) {
+      //  If it catches an exception, we add the error message state
+      this.addErrorMsg(ex.message);
+    }
   }
   openModal() {
     this.setState({
@@ -31,41 +49,79 @@ class NewLogForm extends React.Component {
       selectedDate: this.state.selectedDate,
       validationError: this.state.validationError,
     });
+    return false;
   }
+  addErrorMsg(msg) {
+    const newErrArray = this.state.validationError;
+    newErrArray.push(msg);
+    this.setState({
+      modalIsOpen: this.state.modalIsOpen,
+      selectedDate: this.state.selectedDate,
+      validationError: newErrArray,
+    });
+  }
+  clearErrorMsg() {
+    this.setState({
+      modalIsOpen: this.state.modalIsOpen,
+      selectedDate: this.state.selectedDate,
+      validationError: [],
+    });
+  }
+  updateSelectedDate(date) {
+    this.setState({
+      modalIsOpen: this.state.modalIsOpen,
+      selectedDate: date,
+      validationError: this.state.validationError,
+    });
+  }
+  /**
+   * This function renders the New Log Form ui components:
+   * - A Button that opens a Modal window
+   * - A Modal Window holding:
+   *    - A form (Time Spent, Type of Activity and a Date picker)
+   *    - An error area, shown only when errors are present
+   *    - The action buttons for submission and closing functions
+   **/
   render() {
-    let ErrorArea;
+    let errorArea;
+
+    //  If there's at least one error in the array, we build the error messages DOM Objects
     if (this.state.validationError.length > 0) {
       const errors = this.state.validationError;
       const errorMsgs = errors.map(error => <span className="error-msg">{error}</span>);
-      ErrorArea = <div className="error-area">{errorMsgs}</div>;
+      errorArea = <div className="error-area">{errorMsgs}</div>;
     }
+
+    // Here we create the UI elements
     return (
       <div id="new-log-area">
-        <button onClick={this.openModal}>Add New</button>
+        <button onClick={() => this.openModal()}>Add New</button>
         <Modal
           isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closeModal}
+          onRequestClose={() => this.closeModal()}
           className="new-log-modal"
           overlayClassName="new-log-modal-overlay"
         >
-            <h3></h3>
-            {ErrorArea}
-            <form onSubmit={this.handleFormSubmission} id="new-log-form">
-              <input type="text" id="time" placeholder="Time spent" />
-              <select ref="type" id="type">
-                <option value="run">Running</option>
-                <option value="swim">Swim</option>
-                <option value="bike">Cycling</option>
-              </select>
-              <DatePicker
-                maxDate={moment()}
-                selected={this.state.selectedDate}
-                placeholderText="Date"
-              />
-              <div className="action-buttons">
-                <button type="submit">Submit</button>
-              </div>
-            </form>
+          <h3>New Log</h3>
+          {errorArea}
+          <form onSubmit={this.handleFormSubmission} id="new-log-form">
+            <input type="text" ref="time" id="time" placeholder="Time spent" required />
+            <select ref="type" id="type" required>
+              <option value="run">Running</option>
+              <option value="swim">Swim</option>
+              <option value="bike">Cycling</option>
+            </select>
+            <DatePicker
+              maxDate={moment()}
+              selected={this.state.selectedDate}
+              onChange={this.updateSelectedDate}
+              placeholderText="Date"
+            />
+            <div className="action-buttons">
+              <button type="button" className="secondary" onClick={() => this.closeModal()}>Close</button>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </Modal>
       </div>
     );
