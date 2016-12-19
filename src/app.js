f16c5736112d667ec47c49982ce8e509f5f1f867
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-12-19 08:57:31 -0200

    Added CSS and HTML imports in entry point file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import DOM from 'react-dom';
+import './views/index.html';
+import './scss/index.styl';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import WorkoutApp from './js/components/WorkoutApp.jsx';
 
