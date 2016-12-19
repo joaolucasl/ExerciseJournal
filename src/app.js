b7f70a788f39dd7b73763d3a19b684852b1da0ec
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-12-19 15:27:35 -0200

    Added Provider and Store to App.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import DOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import logReducer from './reducers/logReducer';
+
 import './views/index.html';
 import './scss/index.styl';
 import 'react-datepicker/dist/react-datepicker.css';
 
-import WorkoutApp from './js/components/WorkoutApp.jsx';
+import WorkoutApp from './components/WorkoutApp.jsx';
+
+const store = createStore(logReducer);
 
 DOM.render(
-  <WorkoutApp />,
+  <Provider store={store}>
+    <WorkoutApp />
+  </Provider>,
   document.getElementById('WorkoutApp')
 );
