70592150ceaf794200c6ca7935ba213b3a3115a4
João Lucas Lucchetta <joao.lucas.lucchetta@gmail.com>
2016-12-19 23:32:10 -0200

    Started port to Redux: Removed functions from WorkoutApp and transformed LogList in container

--- src/components/WorkoutApp.jsx
+++ src/components/WorkoutApp.jsx
@@ -1,59 +1,15 @@
 import React from 'react';
-import LogList from './LogList.jsx';
+import LogList from '../containers/LogList.jsx';
 import NewLogForm from './NewLogForm.jsx';
 import LogTimeCounter from './LogTimeCounter.jsx';
 
 class WorkoutApp extends React.Component {
-  /**
-   * The constructor of the Application. Runs the parent's
-   * constructor (super) and sets the inital state.
-   */
-  constructor() {
-    super();
-    this.removeLog = this.removeLog.bind(this);
-    this.addLog = this.addLog.bind(this);
-    this.state = {
-      logs: [],
-    };
-  }
-
-  /**
-   * Adds a new log to the current list,
-   * and updates the Application's state.
-   * @param {Object} log The new log to be added
-   */
-  addLog(log) {
-    const newLogArray = this.state.logs;
-    newLogArray.push(log);
-    this.setState({
-      logs: newLogArray,
-    });
-  }
-  /**
-   * Removes the log corresponding to the UUID passed as parameter
-   * from the array, and updates the state accordingly.
-   * @param {String} logUUID The log's UUID to be removed
-   */
-  removeLog(logUUID) {
-    const updatedLogList = this.state.logs.filter(node => !(node.uuid === logUUID));
-    this.setState({
-      logs: updatedLogList,
-    });
-  }
   render() {
-    let timeCount;
-    if (this.state.logs.length > 0) {
-      timeCount = this.state.logs.reduce((prev, curr) => prev + curr.time, 0);
-    } else {
-      timeCount = 0;
-    }
     return (
       <div className="app-holder">
         <div className="top-area">
-          <LogTimeCounter time={timeCount} />
-          <NewLogForm addLog={this.addLog} />
         </div>
-        <LogList logs={this.state.logs} removeLog={this.removeLog} />
+        <LogList logs={[]} />
       </div>
     );
   }
--- src/containers/LogList.jsx
+++ src/containers/LogList.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import LogItem from './LogItem.jsx';
+import LogItem from '../components/LogItem.jsx';
+import { connect } from 'react-redux';
+import Log from '../classes/Log';
+
 /**
  * This class is the holder for the list of logs.
  * It renders LogItem instances, based off the
@@ -7,25 +10,10 @@ import LogItem from './LogItem.jsx';
  * It forwards a bound `removeLog` function from
  * the parent to each LogItem child.
  */
-class LogList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      logs: this.props.logs || [],
-    };
-    this.removeLog = this.removeLog.bind(this);
-  }
-  componentWillReceiveProps(newProps) {
-    this.setState({
-      logs: newProps.logs,
-    });
-  }
-  removeLog(uuid) {
-    this.props.removeLog(uuid);
-  }
+class LogList extends React.PureComponent {
   render() {
     //  We build the LogItems list based on state
-    const items = this.state.logs.map(log =>
+    const items = this.props.logs.map(log =>
                 <LogItem
                   type={log.type}
                   time={log.time}
@@ -43,8 +31,15 @@ class LogList extends React.Component {
     );
   }
 }
+
 LogList.propTypes = {
-  logs: React.PropTypes.array.isRequired,
-  removeLog: React.PropTypes.func.isRequired,
+  logs: React.PropTypes.arrayOf(Log),
 };
-export default LogList;
+
+const mapStateToProps = (state) => {
+  return {
+    logs: state.logs,
+  };
+};
+
+export default connect(mapStateToProps)(LogList);
